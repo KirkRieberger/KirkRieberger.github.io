0f1817c088819b236b0bfcf5cb868510b6ac7a9e
KirkRieberger <kr17224@gmail.com>
2024-02-19 18:07:19 -0700

    Add dark mode detection listener

--- src/main-index.js
+++ src/main-index.js
@@ -5,9 +5,25 @@ $(document).ready(function () {
     $('.toast').toast('hide');
 })
 
-// if (window.matchMedia("prefers-color-scheme: dark")) {
-//     $("body").addClass("dark-mode");
-// }
+// Dark mode detection
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+// Initial detection
+if (prefersDark.matches) {
+    $("html").attr("data-bs-theme", "dark");
+}
+// Listener Function
+function darkModeTest(event) {
+    if (event.matches) {
+        $("html").attr("data-bs-theme", "dark");
+    } else {
+        $("html").attr("data-bs-theme", "light");
+    }
+}
+// System theme change listener
+prefersDark.addEventListener("change", darkModeTest);
+
+
+
 
 function submitForm() {
     const successModal = $("#form-modal-success");
